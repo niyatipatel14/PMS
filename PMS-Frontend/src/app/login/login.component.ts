621b06df143733d03a83e885f869af600d8d4fca
jaychokshi3178 <105083224+jaychokshi3178@users.noreply.github.com>
2023-02-15 15:51:47 +0530

    made a changes
    made a changes

--- PMS-Frontend/src/app/login/login.component.ts
+++ PMS-Frontend/src/app/login/login.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, } from '@angular/forms';
+import { UserService } from '../register/user.service'; 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
-
+export class LoginComponent implements OnInit {
+  loginForm!: FormGroup;
+  submitted = false;
+  login: any;
+f: any;
+constructor(private formBuilder: FormBuilder, private userservice:UserService) { }
+  ngOnInit(): void {
+    this.loginForm = this.formBuilder.group(
+      {
+        email: ['', [Validators.required,Validators.email, Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')]],
+        password: ['', Validators.required],
+        acceptTerms: [false, Validators.required]
+      },
+    );
+  }
 }
