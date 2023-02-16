cf9582be45d4682803652c99b4075e8ac62ad0a7
jaychokshi3178 <105083224+jaychokshi3178@users.noreply.github.com>
2023-02-16 12:10:25 +0530

    made a changes in forgot page
    made a changes in forgot page

--- PMS-Frontend/src/app/app-routing.module.ts
+++ PMS-Frontend/src/app/app-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ForgotComponent } from './forgot/forgot.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
   {path:'login', component:LoginComponent},
-  {path:'register',component:RegisterComponent}
-
+  {path:'register',component:RegisterComponent},
+  {path:'forgot',component:ForgotComponent}
 ];
 
 @NgModule({
