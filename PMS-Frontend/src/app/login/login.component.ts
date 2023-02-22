bcca25fa674044d94da68fb49a0340e812e9c9ef
jaychokshi3178 <105083224+jaychokshi3178@users.noreply.github.com>
2023-02-22 14:53:34 +0530

    made a changes
    made a changes

--- PMS-Frontend/src/app/login/login.component.ts
+++ PMS-Frontend/src/app/login/login.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, } from '@angular/forms';
 import { Router } from '@angular/router';
-import { UserService } from '../register/user.service'; 
+import { UserService } from '../register/user.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -10,12 +10,12 @@ import { UserService } from '../register/user.service';
 export class LoginComponent implements OnInit {
   loginForm!: FormGroup;
   submitted = false;
-f: any;
-constructor(private formBuilder: FormBuilder, private userservice:UserService,private route: Router) { }
+  f: any;
+  constructor(private formBuilder: FormBuilder, private userservice: UserService, private route: Router) { }
   ngOnInit(): void {
     this.loginForm = this.formBuilder.group(
       {
-        email: ['', [Validators.required,Validators.email, Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')]],
+        email: ['', [Validators.required, Validators.email, Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')]],
         password: ['', Validators.required],
         acceptTerms: [false, Validators.required]
       },
@@ -24,18 +24,17 @@ constructor(private formBuilder: FormBuilder, private userservice:UserService,pr
   hasError(controlName: string, errorName: string) {
     return this.loginForm.controls[controlName].hasError(errorName);
   }
-  login(){
-    const userData={
-      email:this.loginForm.value.email,
-      password:this.loginForm.value.password
+  login() {
+    const userData = {
+      email: this.loginForm.value.email,
+      password: this.loginForm.value.password
     }
-    this.userservice.login(userData).subscribe((data)=>{
-      console.log("userdata",data)
+    this.userservice.login(userData).subscribe((data) => {
+      console.log("userdata", data)
       if (data && data.status == "Success") {
         this.route.navigate(['dashboard'])
-       
       }
-      console.log(data,"user dataa")
+      console.log(data, "user dataa")
     })
   }
 }
